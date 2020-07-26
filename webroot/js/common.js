1ecda33357c6897406951431c13b1c202ae4a3b5
konnohir <sv01.est@gmail.com>
2020-07-26 22:10:27 +0900

    minor change

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -1,6 +1,6 @@
-var common = common || [];
+var app = app || {};
 
-common.paginator = {
+app.paginator = {
     watch: function (suffix) {
         suffix = suffix || '';
         var checkAll = '#checkAll' + suffix;
@@ -32,7 +32,7 @@ common.paginator = {
         var checkRow = '.checkRow' + (suffix || '');
         var $rows = $(checkRow + ':checked');
         if ($rows.length !== 1) {
-            common.alert.error('1件選択してください。');
+            app.modal.error('1件選択してください。');
             return;
         }
 
@@ -51,7 +51,7 @@ common.paginator = {
         var checkRow = '.checkRow' + (suffix || '');
         var $rows = $(checkRow + ':checked');
         if ($rows.length === 0) {
-            common.alert.error('1件以上選択してください。');
+            app.modal.error('1件以上選択してください。');
             return;
         }
 
@@ -64,118 +64,128 @@ common.paginator = {
     }
 }
 
-common.alert = {
+app.modal = {
     info: function (content, callback) {
-        this.modal(content, '情報', 'bg-info', callback, false)
+        this.modal(content, '情報', 'p-2 text-white bg-info', callback, false)
     },
     error: function (content, callback) {
-        this.modal(content, 'エラー', 'bg-danger', callback, false)
+        this.modal(content, 'エラー', 'p-2 text-white bg-danger', callback, false)
     },
     confirm: function (content, okCallback, cancelCallback) {
-        this.modal(content, '確認', 'bg-info', okCallback, cancelCallback)
+        this.modal(content, '確認', 'p-2 text-white bg-info', okCallback, cancelCallback)
     },
-    progress: function (content) {
-        this.modal(content, 'お待ちください', 'bg-info', false, false)
+    confirmDanger: function (content, okCallback, cancelCallback) {
+        this.modal(content, '確認', 'p-2 text-white bg-danger', okCallback, cancelCallback)
+    },
+    progress: function () {
+        this.modal(null, '通信中', 'p-2 text-white bg-info', false, false)
     },
     modal: function (content, title, headerClass, okCallback, cancelCallback) {
         if (!this._modal) {
             this._modal = (
-                $('<div/>').addClass('modal fade').append(
+                $('<div/>').addClass('modal fade').attr('tabindex', -1).append(
                     $('<div/>').addClass('modal-dialog').append(
                         $('<div/>').addClass('modal-content')
-                            .append($('<div/>').addClass('modal-header p-2 text-white'))
+                            .append($('<div/>').addClass('modal-header'))
                             .append($('<div/>').addClass('modal-body'))
-                            .append($('<div/>').addClass('modal-footer justify-content-center')
-                                .append($('<button/>').addClass('btn btn-sm btn-block btn-outline-secondary modal-cancel mt-0').attr('data-dismiss', 'modal').text('キャンセル'))
-                                .append($('<button/>').addClass('btn btn-sm btn-block btn-outline-primary modal-ok mt-0').attr('data-dismiss', 'modal').text('OK'))
-                            )
+                            .append($('<div/>').addClass('modal-footer justify-content-center'))
                     )
                 )
             );
-            $(this._modal).on('shown.bs.modal', function (e) {
-                $('.modal-footer button:not(.d-none):first', this).focus();
+            $(this._modal).on('shown.bs.modal', function () {
+                $('.modal-footer button:first', this).focus();
             });
-            $(this._modal).on('hidden.bs.modal', function (e) {
-                $('.modal-header', this).text(title).removeClass('bg-info bg-warning bg-danger bg-info')
-                $('.modal-cancel', this).off('click').removeClass('d-none');
-                $('.modal-ok', this).off('click').removeClass('d-none');
+            $(this._modal).on('hidden.bs.modal', function () {
+                $('.modal-header', this).empty();
+                $('.modal-body', this).empty();
+                $('.modal-footer', this).empty();
             });
         }
-        if (this._modal.hasClass('show')) {
-            // $(this._modal).one('hidden.bs.modal', function (e) {
-            //     common.alert.modal(content, title, headerClass, okCallback, cancelCallback)
-            // });
-            // return;
+        if ($(this._modal).hasClass('show')) {
             $(this._modal).trigger('hidden.bs.modal');
         }
-        $('.modal-header', this._modal).text(title).addClass(headerClass);
-        $('.modal-body', this._modal).text(content);
-        if (cancelCallback === false) {
-            $('.modal-cancel', this._modal).addClass('d-none');
-        } else if (cancelCallback) {
-            $('.modal-cancel', this._modal).one('click', cancelCallback);
+        $('.modal-header', this._modal).removeClass().addClass('modal-header ' + headerClass).text(title);
+        if (content !== null) {
+            $('.modal-body', this._modal).text(content);
+        } else {
+            var $spinner = $('<div />').addClass('d-block mx-auto text-primary spinner-border');
+            $('.modal-body', this._modal).append($spinner);
         }
-        if (okCallback === false) {
-            $('.modal-ok', this._modal).addClass('d-none');
-        } else if (okCallback) {
-            $('.modal-ok', this._modal).one('click', okCallback);
+        if (cancelCallback !== false) {
+            $('.modal-footer', this._modal).append($('<button/>').addClass('btn btn-sm btn-block btn-outline-secondary modal-cancel mt-0').attr('data-dismiss', 'modal').text('キャンセル'))
+            if (cancelCallback) {
+                $('.modal-cancel', this._modal).one('click', cancelCallback);
+            }
+        }
+        if (okCallback !== false) {
+            $('.modal-footer', this._modal).append($('<button/>').addClass('btn btn-sm btn-block btn-outline-primary modal-ok mt-0').attr('data-dismiss', 'modal').text('OK'))
+            if (okCallback) {
+                $('.modal-ok', this._modal).one('click', okCallback);
+            }
         }
         this._modal.modal({ show: true, backdrop: 'static', keyboard: false });
+        document.activeElement.blur();
     }
 }
 
-common.fallbackErrorHandler = function (response) {
+app.fallbackErrorHandler = function (response) {
     var message = {
         401: '認証が必要です。',
         403: '許可されていない操作です。',
         404: '既に削除されています。',
-        500: 'システムエラーが発生しました。',
     }
-    common.alert.error(message[response.status] || '予期せぬエラーが発生しました。');
+    app.modal.error(message[response.status] || 'システムエラーが発生しました。');
 }
 
-common.createFile = function (fileName, blobData) {
+app.createFile = function (fileName, blobData) {
     if (navigator.msSaveBlob) {
         // IE 10+
-        navigator.msSaveOrOpenBlob(blobData, fileName + ".pdf");
+        navigator.msSaveOrOpenBlob(blobData, fileName);
     } else {
         var blobUrl = URL.createObjectURL(new Blob([blobData], {
             type: blobData.type
         }));
-        var a = $("<a/>").css('display', 'none').attr('href', blobUrl).attr('download', fileName)[0];
+        var a = $('<a/>').css('display', 'none').attr('href', blobUrl).attr('download', fileName)[0];
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
     }
 }
 
+app.parentPage = {
+    watch: function () {
+        // Record index url
+        if ($('.index').length > 0) {
+            sessionStorage.indexUrl = location.href;
+            sessionStorage.viewUrl = '';
+        }
+
+        // Record view url
+        if ($('.view').length > 0) {
+            sessionStorage.viewUrl = location.href;
+        }
+    },
+    get: function (self) {
+        if (sessionStorage.viewUrl && $('.view').length === 0) {
+            return sessionStorage.viewUrl;
+        } else if (sessionStorage.indexUrl) {
+            return sessionStorage.indexUrl;
+        }
+        return $(self).attr('data-action');
+    }
+}
+
 $(function () {
 
     // Watch table
-    common.paginator.watch();
+    app.paginator.watch();
 
-    // Record index url
-    if ($('.index').length) {
-        $(window).bind("beforeunload", function () {
-            sessionStorage.indexUrl = location.href;
-            sessionStorage.viewUrl = '';
-        });
-    }
+    // Watch index, view url
+    app.parentPage.watch();
 
-    // Record view url
-    if ($('.view').length) {
-        sessionStorage.removeItem('viewUrl');
-        $(window).bind("beforeunload", function () {
-            sessionStorage.viewUrl = location.href;
-        });
-    }
-    var $spiner = $('<div />').addClass('modal fade').append($('<div />').addClass('ml-auto text-primary spinner-border float-right'));
-
+    // ajaxStart callback
     $(document).ajaxStart(function () {
-        $spiner.modal('show');
-    });
-    $(document).ajaxStop(function () {
-        $spiner.modal('hide')
+        app.modal.progress();
     });
 
     // Readonly checkbox
@@ -190,7 +200,7 @@ $(function () {
 
     // 汎用ボタン (API)
     $('.btn-jump-api').click(function () {
-        var targets = common.paginator.getTargets(this);
+        var targets = app.paginator.getTargets(this);
         if (targets) {
             $.ajax({
                 url: $(this).attr('data-action'),
@@ -199,8 +209,8 @@ $(function () {
                 headers: { 'X-CSRF-TOKEN': $('#postForm [name=_csrfToken]').val() },
                 data: { targets: targets },
             }).fail(function (response) {
-                common.fallbackErrorHandler(response);
-            }).always(function (response) {
+                app.fallbackErrorHandler(response);
+            }).done(function (response) {
                 location.reload();
             });
         }
@@ -208,7 +218,7 @@ $(function () {
 
     // 編集ボタン
     $('.btn-edit').click(function () {
-        var id = common.paginator.getId(this);
+        var id = app.paginator.getId(this);
         if (id) {
             location.href = $(this).attr('data-action') + '/' + id;
         }
@@ -216,32 +226,35 @@ $(function () {
 
     // 削除ボタン
     $('.btn-delete').click(function () {
-        var targets = common.paginator.getTargets(this);
+        var targets = app.paginator.getTargets(this);
         if (targets) {
-            $.ajax({
-                url: $(this).attr('data-action'),
-                method: 'post',
-                dataType: 'json',
-                headers: { 'X-CSRF-TOKEN': $('#postForm [name=_csrfToken]').val() },
-                data: { targets: targets },
-            }).fail(function (response) {
-                common.fallbackErrorHandler(response);
-            }).always(function (response) {
-                if ($('.btn-cancel').length) {
-                    $('.btn-cancel').click();
-                } else {
-                    location.reload();
-                }
+            var url = $(this).attr('data-action')
+            app.modal.confirmDanger('削除します。よろしいですか？', function () {
+                $.ajax({
+                    url: url,
+                    method: 'post',
+                    dataType: 'json',
+                    headers: { 'X-CSRF-TOKEN': $('#postForm [name=_csrfToken]').val() },
+                    data: { targets: targets },
+                }).fail(function (response) {
+                    app.fallbackErrorHandler(response);
+                }).done(function () {
+                    if ($('.btn-cancel').length > 0) {
+                        $('.btn-cancel').click();
+                    } else {
+                        location.reload();
+                    }
+                });
             });
         }
     });
 
     // パスワード再発行ボタン
     $('.btn-password-issue').click(function () {
-        var targets = common.paginator.getTargets(this);
+        var targets = app.paginator.getTargets(this);
         if (targets) {
             var url = $(this).attr('data-action')
-            common.alert.confirm('発行します。よろしいですか？', function () {
+            app.modal.confirm('発行します。よろしいですか？', function () {
                 $.ajax({
                     url: url,
                     method: 'post',
@@ -249,11 +262,14 @@ $(function () {
                     headers: { 'X-CSRF-TOKEN': $('#postForm [name=_csrfToken]').val() },
                     data: { targets: targets },
                 }).fail(function (response) {
-                    common.fallbackErrorHandler(response);
+                    app.fallbackErrorHandler(response);
                 }).done(function (response) {
-                    common.createFile('password.csv', response);
-                }).always(function (response) {
-                    location.reload();
+                    app.createFile('password.csv', response);
+                }).done(function () {
+                    // setTimeout for IE
+                    setTimeout(function () {
+                        location.reload();
+                    }, 0);
                 });
             });
         }
@@ -261,28 +277,15 @@ $(function () {
 
     // キャンセルボタン
     $('.btn-cancel').click(function () {
-        if (sessionStorage.viewUrl) {
-            location.href = sessionStorage.viewUrl;
-            sessionStorage.removeItem('viewUrl');
-        } else if (sessionStorage.indexUrl) {
-            location.href = sessionStorage.indexUrl;
-            sessionStorage.removeItem('indexUrl');
-        } else {
-            location.href = $(this).attr('data-action');
-        }
-    });
-
-    // 一覧画面へ戻るボタン
-    $('.btn-back-to-index').click(function () {
-        var url = sessionStorage.indexUrl;
-        sessionStorage.removeItem('viewUrl');
-        sessionStorage.removeItem('indexUrl');
-        location.href = url || $(this).attr('data-action');
+        location.href = app.parentPage.get(this);
     });
 
     // フォーム送信ボタン
     $('.btn-submit').click(function () {
-        $(this).parents('form').submit();
+        var $target = $(this).parents('form');
+        app.modal.confirm('送信します。よろしいですか？', function () {
+            $target.submit();
+        });
     });
 
 });
